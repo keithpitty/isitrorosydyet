ed981ac8f05c5fa993bf3e096392166cbe2f5bb1
Keith Pitty <keith@keithpitty.com>
2009-05-26 14:03:30 +1000

    Initial commit

---
+++ isitrorosydyet.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+helpers do
+  def days_until_meetup
+    now = Time.now.utc
+    @meetup = Time.utc(2009, 6, 9, 15, 0, 0)
+    ((@meetup - now) / (60 * 60 * 24)).to_i
+  end
+end
+
+get "/" do
+  @days = days_until_meetup
+  case
+  when @days > 0
+    @big = 'NO'
+    @small = "only #{@days} sleep#{@days > 1 ? 's' : ''} to go... see you at the Trinity on #{@meetup.strftime("%A, %d %B")}"
+  when @days == 0
+    @big = 'YES'
+    @small = '#rorosyd is on today!'
+  else
+    @big = 'NO'
+    @small = '#rorosyd is over for another month :('
+  end
+  haml :index
+end
