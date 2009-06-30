40226df444c740989183cd0a5bd2e8e31f322cad
Keith Pitty <keith@keithpitty.com>
2009-06-30 12:20:28 +1000

    A few changes in anticipation of July meetup.

--- isitrorosydyet.rb
+++ isitrorosydyet.rb
@@ -6,7 +6,7 @@ require 'activesupport'
 helpers do
   def days_until_meetup
     now = Time.now.utc
-    @meetup = Time.utc(2009, 6, 9, 15, 0, 0)
+    @meetup = Time.utc(2009, 7, 14, 15, 0, 0)
     ((@meetup - now) / (60 * 60 * 24)).to_i
   end
 end
@@ -17,12 +17,15 @@ get "/" do
   when @days > 0
     @big = 'NO'
     @small = "only #{@days} sleep#{@days > 1 ? 's' : ''} to go... see you at the Trinity on #{@meetup.strftime("%A, #{@meetup.to_date.day.ordinalize} %B")}"
+    @details = false
+    @need_more_talks = false
   when @days == 0
     @big = 'YES'
     @small = '#rorosyd is on today!'
   else
     @big = 'NO'
     @small = '#rorosyd is over for another month :('
+    @over = true
   end
   @plug = 'a <a href="http://bivou.ac">bivou.ac</a> service'
   haml :index, :options => {:format => :html5,
