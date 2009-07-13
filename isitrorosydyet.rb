0f621239ac1f61ef9a583d586db23f9e3d4f7034
Keith Pitty <keith@keithpitty.com>
2009-07-14 09:09:40 +1000

    Fixed slight but important logic flaw

--- isitrorosydyet.rb
+++ isitrorosydyet.rb
@@ -13,12 +13,12 @@ end
 
 get "/" do
   @days = days_until_meetup
+  @details = true
   case
   when @days > 0
     @big = 'NO'
     trinity_map_link = "<a href='http://maps.google.com.au/maps?f=q&source=s_q&hl=en&geocode=&q=trinity+bar+surry+hills&vps=1&jsv=165c&sll=-25.335448,135.745076&sspn=40.257673,71.894531&num=10&iwloc=A'>the Trinity</a>"
     @small = "only #{@days} sleep#{@days > 1 ? 's' : ''} to go... see you at #{trinity_map_link} on #{@meetup.strftime("%A, #{@meetup.to_date.day.ordinalize} %B")}"
-    @details = true
     @need_more_talks = false
   when @days == 0
     @big = 'YES'
